6d7e162a688748b7c43a259fd24f4833e8972e5e
Franz Noel Tanglao <franz@tanglao.org>
2022-06-04 17:00:58 -0700

    feat: Add husky, linter, and prettier

---
+++ graphql-server/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    "eol-last": "error",
+    "no-return-await": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "no-console": "error"
+  }
+}
