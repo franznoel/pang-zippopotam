211513623738fd735ed1e95d3ce0b6f2b7497327
Franz Noel Tanglao <franz@tanglao.org>
2022-06-05 18:59:42 -0700

    feat: Design UX for searching

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,23 +1,58 @@
+import { Autocomplete, Button, Grid, Icon, IconButton, TextField, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { makeStyles } from '@mui/styles';
+import { Container } from '@mui/system';
+import { countryCodes } from './data/countryCodes';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const useStyles = makeStyles({
+  App: {
+    textAlign: 'center',
+    margin: '20px auto',
+  },
+  Container: {
+    border: '0 solid #afafaf',
+    borderRadius: '5px',
+    padding: '10px',
+    boxShadow: '0 0 20px #afafaf'
+  },
+  SearchButton: {
+    width: '100%',
+    height: '100%'
+  },
+  postal: {
+    width: '100%'    
+  }
+})
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
+    <div className={classes.App}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container maxWidth="lg" className={classes.Container}>
+          <Typography variant="h1">Pang Zippopotam</Typography>
+          <Grid container spacing={2} padding={5}>
+            <Grid item xs={5}>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={countryCodes}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Country" />}
+                defaultValue={countryCodes.find((countryCode)=> countryCode.code === 'US')}
+                style={{ width: '100%' }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField id="outlined-basic" label="Zip Code/Postal Code" variant="outlined" className={classes.postal}/>
+            </Grid>
+            <Grid item xs={3}>
+              <Button variant="contained" size="large" startIcon={<SearchIcon />} className={classes.SearchButton}>Search</Button>
+            </Grid>
+          </Grid>
+        </Container>
       </header>
     </div>
   );
